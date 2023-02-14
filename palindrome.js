509c68f2eaf31fc1da056962e8fc5ac609d086f5
VamsheeKrishna51 <vamshiinfo564@gmail.com>
2023-02-15 00:10:54 +0530

    fixed the code

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
 let str = 1221;
 let number = str;
 let sum = '';
-while(number>=0){
+while(number>0){
 let m = number%10;
 sum +=m;
-number =(number/10);
+number =Math.floor(number/10);
 }
 if(sum == str){
 console.log("palindrome");
